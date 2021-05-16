cce6645f678cfcdde51cbc2721d1a033a63c9b42
Bojan Majstorovic <majstor1902@gmail.com>
2021-05-16 15:46:52 +0200

    Updated valgrind.sh script contents with the new path to the ThesaurusApp binary

--- valgrind.sh
+++ valgrind.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./testApp
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./build/bin/ThesaurusApp
 
